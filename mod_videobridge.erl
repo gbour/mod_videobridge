8d8c50f756271bc8e4c7ad47591b5d9547c0df25
Guillaume Bour <guillaume@bour.cc>
2013-05-20 22:57:03 +0200

    [feat] handle conference channels allocation/freeing (NOTE: faking RTP ports allocation)

--- mod_videobridge.erl
+++ mod_videobridge.erl
@@ -24,8 +24,21 @@
 -define(SUBDOMAIN, <<"jitsi-videobridge">>).
 -define(NS_COLIBRI, <<"http://jitsi.org/protocol/colibri">>).
 -record(state, {
-		host
-	}).
+    host
+  }).
+
+-record(channel, {
+    id,
+	confid,
+	type,
+	host,
+	rtpport,
+	rtcpport
+  }).
+%-record(conference, {
+%    id
+%  }).
+
 
 start_link(Host, Opts) ->
     Proc = gen_mod:get_module_proc(Host, ?MODULE),
@@ -50,6 +63,10 @@ stop(Host) ->
 init([Host, Opts]) ->
 	Fqdn = fqdn(Host),
 	?DEBUG("videobridge:init:: ~p, ~p~n", [Fqdn, Opts]),
+
+	%
+	random:seed(),
+	ets:new(videobridge_channel, [bag,named_table,public]),
 
 	% discovery hooks
 	%mod_disco:register_feature(Host, ?NS_COLIBRI),
@@ -109,7 +126,84 @@ do_route(From, To, IQ=#iq{type=get, xmlns=?NS_DISCO_INFO}) ->
 		empty, [From, To, Node, Lang]),
 	?DEBUG("disco: host= ~p, node= ~p, res= ~p~n", [Host,Node,Res]),
 
-	mod_disco:process_local_iq_info(From, To, IQ).
+	mod_disco:process_local_iq_info(From, To, IQ);
+
+do_route(From, To, #iq{xmlns=?NS_COLIBRI, sub_el=El}) when
+		El#xmlel.name =:= <<"conference">> ->
+%		sub_el=#xmlel{name=<<"conference">>, attrs=Attrs, children=Elts}}) ->
+	?DEBUG("videobridge: conference query~n",[]),
+	%NOTE: if no id, ConfId == <<"">>
+	ConfId  = case xml:get_tag_attr_s(<<"id">>, El) of
+		<<"">> -> random:uniform(99999);
+		_Id    -> jlib:binary_to_integer(_Id)
+	end,
+	%TODO: case no content
+	Content = xml:get_subtag(El, <<"content">>),
+	?DEBUG("channels= ~p~n", [Content#xmlel.children]),
+
+	% audio or video
+	Type = xml:get_tag_attr_s(<<"name">>, Content),
+	Channels = do_channels([], Content#xmlel.children, {ConfId, Type}),
+
+	#iq{type=result, sub_el=[
+		#xmlel{name= <<"conference">>, 
+			attrs=[{<<"xmlns">>, ?NS_COLIBRI}, {<<"id">>, jlib:integer_to_binary(ConfId)}],
+			children=[
+				#xmlel{name= <<"content">>, attrs=[{<<"name">>,Type}], children=Channels}
+			]
+		}
+	]};
+
+do_route(_,_,IQ) ->
+	?DEBUG("videobridge: unknown message ~p~n", [IQ]),
+	#iq{type=error, sub_el=[jlib:make_error_element(<<"404">>, <<"unknown msg">>)]}.
+
+do_channels(Acc, [],_) ->
+	Acc;
+do_channels(Acc, [El=#xmlel{name= <<"channel">>}|T], Opts) ->
+	{Action, ChanId} = case xml:get_tag_attr_s(<<"id">>, El) of
+		<<"">> -> {allocate, undef};
+		Id     ->
+			Id2 = jlib:binary_to_integer(Id),
+			case xml:get_tag_attr_s(<<"expire">>, El) of
+				<<"0">> -> {free, Id};
+				_       -> {update, Id}
+			end
+	end,
+	do_channels([channel(Action, ChanId, Opts)|Acc], T, Opts);
+do_channels(Acc,[_|T],Opts) ->
+	do_channels(Acc, T, Opts).
+
+channel(allocate, undef, {ConfId, Type}) ->
+	ChanId=random:uniform(9999),
+	?DEBUG("channel: alloc(~p)~n", [ChanId]),
+	Chan = #channel{id=ChanId, confid=ConfId, type=Type, host= <<"0.0.0.0">>, 
+		rtpport=random:uniform(9999), 
+		rtcpport=random:uniform(9999)
+	},
+	ets:insert(videobridge_channel, {ChanId, Chan}),
+	
+	#xmlel{name= <<"channel">>, attrs=[
+		{<<"id">>      , jlib:integer_to_binary(ChanId)},
+		{<<"host">>    , Chan#channel.host},
+		{<<"rtpport">> , jlib:integer_to_binary(Chan#channel.rtpport)},
+		{<<"rtcpport">>, jlib:integer_to_binary(Chan#channel.rtcpport)},
+		{<<"expire">>  , <<"60">>}
+	]};
+
+channel(free    , ChanId, _)              ->
+	?DEBUG("channel: free(~p)~n", [ChanId]),
+	case ets:lookup(videobridge_channel, ChanId) of 
+		[{ChanId, Chan}] -> 
+			ets:delete(videobridge_channel, ChanId);
+		_                -> 
+			?DEBUG("channel(free): ERROR, ~p channel not found~n", [ChanId])
+	end,
+	{xmlcdata,<<"">>};
+
+channel(update  , ChanId, _)              ->
+	?DEBUG("channel: update(~p)~n", [ChanId]),
+	update.
 
 %
 handle_call(stop, _From, State) ->
