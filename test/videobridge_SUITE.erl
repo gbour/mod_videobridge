20e6122b2d7c6f74ce9854091a8c78192964b068
Guillaume Bour <guillaume@bour.cc>
2013-09-23 22:24:09 +0200

    [test] videobridge capability unittest #2 (disco_info)

--- test/videobridge_SUITE.erl
+++ test/videobridge_SUITE.erl
@@ -2,7 +2,6 @@
 -module(videobridge_SUITE).
 -compile(export_all).
 
-%-include_lib("escalus.hrl").
 -include_lib("escalus/include/escalus.hrl").
 -include_lib("exml/include/exml.hrl").
 -include_lib("common_test/include/ct.hrl").
@@ -12,10 +11,13 @@
 %%--------------------------------------------------------------------
 
 all() ->
-    [{group, messages}].
+    [{group, disco}].
 
 groups() ->
-    [{messages, [sequence], [capabilities]}].
+    [{disco, [sequence], [
+		disco_items,
+		disco_info
+	]}].
 
 suite() ->
     escalus:suite().
@@ -44,19 +46,33 @@ end_per_testcase(CaseName, Config) ->
     escalus:end_per_testcase(CaseName, Config).
 
 %%--------------------------------------------------------------------
-%% Message tests
+%% tests
 %%--------------------------------------------------------------------
 
-capabilities(Config) ->
-	escalus:story(Config, [1,1], fun(Alice, Bob) ->
+disco_items(Config) ->
+	escalus:story(Config, [1], fun(Alice) ->
 		% TOO BAD: Alice jid is a binary string
 		% so we have to extract domain from Config
 		Domain = proplists:get_value(server,
-			proplists:get_value(alice, 
+			proplists:get_value(alice,
 				proplists:get_value(escalus_users, Config))),
 
 		R = escalus:send_and_wait(Alice, escalus_stanza:disco_items(Domain)),
 		io:format("R= ~p~n~p~n", [Domain, R]),
 
 		escalus:assert(has_item, [<<"jitsi-videobridge.", Domain/binary>>], R)
+	end).
+
+disco_info(Config) ->
+	escalus:story(Config, [1], fun(Alice) ->
+		Domain = proplists:get_value(server,
+			proplists:get_value(alice,
+				proplists:get_value(escalus_users, Config))),
+
+		R = escalus:send_and_wait(Alice,
+			escalus_stanza:disco_info(<<"jitsi-videobridge.", Domain/binary>>)),
+		io:format("R= ~p~n~p~n", [Domain, R]),
+
+		escalus:assert(has_identity, [<<"component">>, <<"conference">>], R),
+		escalus:assert(has_feature, [<<"http://jitsi.org/protocol/colibri">>], R)
 	end).
